e8d3ee34d544a81f96b54c0fdbf415d863a6afde
aitechdev79 <aitechdev79@gmail.com>
2026-03-05 03:06:28 +0700

    Update about card image

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -35,7 +35,7 @@ export default function AboutSection() {
       title: 'Tầm nhìn, sứ mệnh, mục tiêu',
       description: 'Cam kết cung cấp thông tin minh bạch và đáng tin cậy để xây dựng tương lai bền vững',
       href: '/commitment',
-      image: '/camket.jpeg',
+      image: '/nong dan duoc mua.jpg',
       accentColor: 'vn-green',
     },
     {
@@ -152,4 +152,5 @@ export default function AboutSection() {
     </section>
   );
 }
+
 
